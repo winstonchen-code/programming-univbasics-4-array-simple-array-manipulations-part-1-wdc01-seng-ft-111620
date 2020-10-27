f358d699b7fbb30987a3c2ebcb9a5daeb9b24d36
Winston Chen <winstonchen.wm@gmail.com>
2020-10-27 04:52:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  
